0c7b6eac1e9d0d66001a36145903dafff14d4863
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-03-06 11:42:46 -0300

    escondendo o formulario e adicionando tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,20 +1,18 @@
-// encontrar botao add tarefa
-
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
-const tarefas = []
+const ulTarefas = document.querySelector('.app__section-task-list');
+
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function criarElementoTarefa(tarefa){
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
     const svg = document.createElement('svg');
     svg.innerHTML = `
-        <svg>
-            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-                <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-            </svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
     `
 
@@ -24,12 +22,14 @@ function criarElementoTarefa(tarefa){
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
     const imagemBotao = document.createElement('img');
-    imagemBotao.setAttribute('src', '/imagens/edit.png');
+    imagemBotao.setAttribute('src', './imagens/edit.png');
     botao.append(imagemBotao);
 
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    return li;
 }
 
 btnAdicionarTarefa.addEventListener('click', () => {
@@ -41,6 +41,14 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     const tarefa = {
         descricao: textArea.value
     }
+    formAdicionarTarefa.classList.add('hidden')
     tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa)
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
-})
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+});
